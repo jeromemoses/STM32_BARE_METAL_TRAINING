86e51394744452dd8bbfc1e7cda145e07575b2d8
jeromemoses <jeromemoses05@gmail.com>
2024-04-18 00:08:30 +0530

    Input Interrupt driver

---
+++ 08_Input_Interrupt_driver/Inc/exti.h
@@ -0,0 +1,13 @@
+/*
+ * exti.h
+ *
+ *  Created on: Apr 17, 2024
+ *      Author: jerom
+ */
+
+#ifndef EXTI_H_
+#define EXTI_H_
+
+
+
+#endif /* EXTI_H_ */
---
+++ 08_Input_Interrupt_driver/Src/main.c
@@ -0,0 +1,9 @@
+#include "stm32f0xx.h"
+#include "stm32f091xc.h"
+#include <stdint.h>
+
+
+int main(void)
+{
+
+}
